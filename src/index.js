2053dfa8ee74663eeac266e22df8d5012d1187a1
Jorge Molina <103226501+Jorgemagus@users.noreply.github.com>
2023-04-11 10:14:44 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors())
-const whisteList = ["http://localhost:3000", "https://new-backendrepo-production.up.railway.app/"];
+const whisteList = ["http://localhost:3000"];
 app.all(whisteList, (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allowed-Method', 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH');
